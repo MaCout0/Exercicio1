7d22d95e40e976829f75e470a6a494686ffc1689
MaCout0 <jkmbint@gmail.com>
2024-03-22 18:59:41 -0300

    adicionando o restante das operações e complletando o menu.

--- Exercicio1/Program.cs
+++ Exercicio1/Program.cs
@@ -18,29 +18,39 @@
             {
                 Console.Clear();
 
-                Console.WriteLine("Hello, World!");
-
-                 choose = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("             Calculadora");
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("Selecione uma das seguintes opções: \n");
+                Console.WriteLine("1 - Soma");
+                Console.WriteLine("2 - Subtração");
+                Console.WriteLine("3 - Multipicação");
+                Console.WriteLine("4 - Divisão");
+                Console.WriteLine("5 - Sair");
+                Console.Write("Opção: ");
+                choose = Convert.ToInt32(Console.ReadLine());
                 
                 Console.Clear();
 
                 switch (choose)
                 {
                     case 1:
+                        
                         soma();
                         break;
 
                     case 2:
-
-                    break;
+                        
+                        subtracao();
+                        break;
 
                     case 3:
-
-                    break;
+                        multiplicacao();
+                        break;
 
                     case 4:
-
-                    break;
+                        divisao();
+                        break;
                 }
             }
 
@@ -55,9 +65,49 @@
             Console.WriteLine("Digite o segundo valor: ");
             valor2 = Convert.ToInt32(Console.ReadLine());
             result = valor1 + valor2;
-            Console.WriteLine($"{valor1} + {valor2} = {result}");
+            Console.WriteLine($"\nO resultado da operação é: {valor1} + {valor2} = {result} \n\npressione qualquer tecla para voltar ao menu!!");
             Console.ReadLine();
 
         }
+
+        static void subtracao()
+        {
+            int valor1, valor2, result;
+            Console.WriteLine("Digite o primeiro valor: ");
+            valor1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o segundo valor: ");
+            valor2 = Convert.ToInt32(Console.ReadLine());
+            result = valor1 - valor2;
+            Console.WriteLine($"\nO resultado da operação é: {valor1} - {valor2} = {result} \n\npressione qualquer tecla para voltar ao menu!!");
+            Console.ReadLine();
+        }
+
+        static void multiplicacao()
+        {
+            int valor1 = 0, valor2 = 0, result = 0;
+            Console.WriteLine("Digite o primeiro valor: ");
+            valor1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o segundo valor: ");
+            valor2 = Convert.ToInt32(Console.ReadLine());
+            result = valor1 * valor2;
+            Console.WriteLine($"\nO resultado da operação é: {valor1} * {valor2} = {result} \n\npressione qualquer tecla para voltar ao menu!!");
+            Console.ReadLine();
+        }
+
+        static void divisao()
+        {
+            int valor1 = 0, valor2 = 0, result = 0;
+            Console.WriteLine("Digite o primeiro valor: ");
+            valor1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o segundo valor: ");
+            valor2 = Convert.ToInt32(Console.ReadLine());
+            result = valor1 / valor2;
+            Console.WriteLine($"\nO resultado da operação é: {valor1} / {valor2} = {result} \n\npressione qualquer tecla para voltar ao menu!!");
+            Console.ReadLine();
+        }
+
+        
+
+        
     }
 }
